a247350345d1215c65cc4906eaf2db931dce6135
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2025-03-16 23:35:43 +0530

    update push

--- client/src/pages/Sell/index.js
+++ client/src/pages/Sell/index.js
@@ -65,53 +65,68 @@ const SellNFT = () => {
 
         if (typeof accounts === undefined) {
           alert("Please login with Metamask!");
-          return;
+          console.log("login to metamask");
         }
 
         const networkId = await web3.eth.net.getId();
-        const artTokenContract = new web3.eth.Contract(
-          ArtToken.abi,
-          ArtToken.networks[networkId].address
-        );
-        const marketplaceContract = new web3.eth.Contract(
-          ArtMarketplace.abi,
-          ArtMarketplace.networks[networkId].address
-        );
+        try {
+          const artTokenContract = new web3.eth.Contract(
+            ArtToken.abi,
+            ArtToken.networks[networkId].address
+          );
+          // console.log("Contract: ", artTokenContract);
+          const marketplaceContract = new web3.eth.Contract(
+            ArtMarketplace.abi,
+            ArtMarketplace.networks[networkId].address
+          );
+          const totalSupply = await artTokenContract.methods
+            .totalSupply()
+            .call();
+          const totalItemsForSale = await marketplaceContract.methods
+            .totalItemsForSale()
+            .call();
 
-        const totalSupply = await artTokenContract.methods.totalSupply().call();
-        const totalItemsForSale = await marketplaceContract.methods.totalItemsForSale().call();
+          for (var tokenId = 1; tokenId <= totalSupply; tokenId++) {
+            let item = await artTokenContract.methods.Items(tokenId).call();
+            let owner = await artTokenContract.methods.ownerOf(tokenId).call();
 
-        for (let tokenId = 1; tokenId <= totalSupply; tokenId++) {
-          let item = await artTokenContract.methods.Items(tokenId).call();
-          let owner = await artTokenContract.methods.ownerOf(tokenId).call();
+            const response = await api
+              .get(`/tokens/${tokenId}`)
+              .catch((err) => {
+                console.log("Err: ", err);
+              });
+            console.log("response: ", response);
 
-          const response = await api.get(`/tokens/${tokenId}`).catch((err) => {
-            console.log("Err: ", err);
-          });
+            itemsList.push({
+              name: response.data.name,
+              description: response.data.description,
+              image: response.data.image,
+              tokenId: item.id,
+              creator: item.creator,
+              owner: owner,
+              uri: item.uri,
+              isForSale: false,
+              saleId: null,
+              price: 0,
+              isSold: null,
+            });
+          }
+          if (totalItemsForSale > 0) {
+            for (var saleId = 0; saleId < totalItemsForSale; saleId++) {
+              let item = await marketplaceContract.methods
+                .itemsForSale(saleId)
+                .call();
+              let active = await marketplaceContract.methods
+                .activeItems(item.tokenId)
+                .call();
 
-          itemsList.push({
-            name: response.data.name,
-            description: response.data.description,
-            image: response.data.image,
-            tokenId: item.id,
-            creator: item.creator,
-            owner: owner,
-            uri: item.uri,
-            isForSale: false,
-            saleId: null,
-            price: 0,
-            isSold: null,
-          });
-        }
+              let itemListIndex = itemsList.findIndex(
+                (i) => i.tokenId === item.tokenId
+              );
 
-        if (totalItemsForSale > 0) {
-          for (let saleId = 0; saleId < totalItemsForSale; saleId++) {
-            let item = await marketplaceContract.methods.itemsForSale(saleId).call();
-            let active = await marketplaceContract.methods.activeItems(item.tokenId).call();
 
-            let itemListIndex = itemsList.findIndex((i) => i.tokenId === item.tokenId);
 
-            if (itemListIndex !== -1) {
+
               itemsList[itemListIndex] = {
                 ...itemsList[itemListIndex],
                 isForSale: active,
@@ -121,23 +136,43 @@ const SellNFT = () => {
               };
             }
           }
+
+          dispatch(setAccount(accounts[0]));
+          dispatch(setTokenContract(artTokenContract));
+          dispatch(setMarketContract(marketplaceContract));
+          dispatch(setNft(itemsList));
+        } catch (error) {
+          console.error("Error", error);
+          alert(
+            "Contracts not deployed to the current network " +
+              networkId.toString()
+          );
         }
 
-        dispatch(setAccount(accounts[0]));
-        dispatch(setTokenContract(artTokenContract));
-        dispatch(setMarketContract(marketplaceContract));
-        dispatch(setNft(itemsList));
+
+
+
+
       } catch (error) {
-        console.error("Initialization error:", error);
-        alert("Error loading blockchain data.");
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.` +
+            error
+        );
+        console.error(error);
       }
     };
 
     init();
   }, [dispatch]);
 
-  const ownedNfts = nft.filter((item) => item.owner.toLowerCase() === account?.toLowerCase());
+  console.log("Nft :", nft);
 
+  const nftItem = useSelector((state) => state.allNft.nft);
+  
+  // Filter out NFTs owned by the current user
+  const ownedNfts = nftItem.filter((item) => 
+    item.owner.toLowerCase() === account?.toLowerCase()
+  );
   return (
     <div style={styles.container}>
       <div style={styles.section}>
